9cb10c49928f37a4a20a066118b065a44e4080ac
C K <ctkoo@ucsc.edu>
2022-07-26 00:10:19 -0700

    heroku deployment is a pain

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 //Import express
 const express = require('express')
 
-const PORT = 3001;
+const fs = require('fs');
+
+const dbNotes = require('./db/db.json');
+
+const PORT = process.env.PORT ||3001;
 
 const app = express();
+
+// Route to the notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__filename, '/db/db.json'))
+
+);
+
+app.post('/api/notes', (req, res) =>
+  
+
+);
+
+app.get('*',(req,res)=>
+  res.sendFile(path.join(__dirname,'/public/index.html'))
+);
 
 //To initialize the server
 app.listen(PORT, () =>
