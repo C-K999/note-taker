64666a23669c3b4823710fdc515d0f034fe4c830
C K <ctkoo@ucsc.edu>
2022-07-26 00:43:26 -0700

    so that's how express.static works

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT ||3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
 // Route to the notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
