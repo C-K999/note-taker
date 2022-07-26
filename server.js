4e1a8f4fbbf03b904740c98dcc4772291623aaa6
C K <ctkoo@ucsc.edu>
2022-07-26 00:26:42 -0700

    got local server deployment to work, updated that unseemly notes.html

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //Import express
 const express = require('express')
 
+const path = require('path');
+
 const fs = require('fs');
 
 const dbNotes = require('./db/db.json');
@@ -19,13 +21,8 @@ app.get('/api/notes', (req, res) =>
 
 );
 
-app.post('/api/notes', (req, res) =>
-  
-
-);
-
-app.get('*',(req,res)=>
-  res.sendFile(path.join(__dirname,'/public/index.html'))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 //To initialize the server
